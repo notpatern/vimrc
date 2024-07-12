408a0ec2dee57f6f3e8ed2bb445e68760aecc529
Sasha <132772266+notpatern@users.noreply.github.com>
2024-07-12 18:33:24 +0200

    initial commit

---
+++ lua/notpatern/lazy/init.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    "nvim-neotest/nvim-nio",
+    "github/copilot.vim",
+    "eandrju/cellular-automaton.nvim",
+    "gpanders/editorconfig.nvim",
+    { -- This plugin
+        "Zeioth/compiler.nvim",
+        cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+        dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+        opts = {},
+    },
+    { -- The task runner we use
+        "stevearc/overseer.nvim",
+        commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+        cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+        opts = {
+            task_list = {
+                direction = "bottom",
+                min_height = 25,
+                max_height = 25,
+                default_detail = 1
+            },
+        },
+    },
+}
+
